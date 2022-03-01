1246765d1580b19e744f556f7283fad17c02853c
nparigi-ledger <nicolas.parigi@ledger.fr>
2022-03-01 18:26:17 +0100

    wip

--- src/screens/Portfolio/index.tsx
+++ src/screens/Portfolio/index.tsx
@@ -37,6 +37,7 @@ import DiscoverSection from "./DiscoverSection";
 import AddAssetsCard from "./AddAssetsCard";
 import Assets from "./Assets";
 import MarketSection from "./MarketSection";
+import MarketTopGainersDataProviderWrapper from "./MarketTopGainersDataProviderWrapper";
 
 export { default as PortfolioTabIcon } from "./TabIcon";
 
@@ -192,12 +193,12 @@ export default function PortfolioScreen({ navigation }: Props) {
           ]
         : []),
       <Flex mx={6} mt={10}>
-        <SectionTitle
-          title={<Trans i18nKey={"tabs.platform"} />}
-          navigation={navigation}
-          navigatorName={NavigatorName.Market}
-        />
-        <MarketSection />
+          <SectionTitle
+            title={<Trans i18nKey={"tabs.platform"} />}
+            navigation={navigation}
+            navigatorName={NavigatorName.Market}
+          />
+          <MarketSection />
       </Flex>,
       <Box mt={24} />,
     ],
